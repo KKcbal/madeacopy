831ea2b5938297946c0f9377af74e996107b20ac
kkcbal <tabbed.out3321@gmail.com>
2022-09-09 15:23:35 -0700

    pytontest

---
+++ _posts/pythonetst.py
@@ -0,0 +1,10 @@
+from tkinter.messagebox import YES
+
+
+print("this is a test to see how python looks without a jupyter notebook's limits")
+psr=input("does input work?")
+
+if psr == "yes":
+    print("thats cool")
+else:
+    print("that's too bad")
